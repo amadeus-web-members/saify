6ae369aaf0677120b74d554dc94f1d9120ff902c
Imran Ali Namazi <imran@amadeusweb.world>
2026-01-11 16:50:19 +0530

    setup

---
+++ cms.php
@@ -0,0 +1,7 @@
+<?php
+variables([
+	'social-separator' => 'newline', //-with-hr
+	'social' => array_merge([
+		[ 'type' => 'linkedin', 'url' => 'https://in.linkedin.com/in/saify-saraiya', 'name' => 'Saify Saraiya'],
+	], main::defaultSocial()),
+]);
